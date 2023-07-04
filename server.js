102dbdaad3b9bf818f9149120572346d074f8d50
Shreya Tripathi <88155467+shreya27tripathi@users.noreply.github.com>
2023-07-05 01:08:09 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,72 @@
+const { Client } = require('@elastic/elasticsearch');
+const client = require('./elasticsearch/client');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const data = require('./data_management/retrieve_and_ingest_data');
+
+app.use('/ingest_data', data);
+
+app.use(cors());
+
+app.get('/results', (req, res) => {
+  const passedType = req.query.type;
+  const passedMag = req.query.mag;
+  const passedLocation = req.query.location;
+  const passedDateRange = req.query.dateRange;
+  const passedSortOption = req.query.sortOption;
+
+  async function sendESRequest() {
+
+    const body = await client.search({
+      index: 'earthquakes',
+      body: {
+        sort: [
+          {
+            mag: {
+              order: passedSortOption,
+            },
+          },
+        ],
+        size: 300,
+        query: {
+          bool: {
+            filter: [
+              {
+                term: { type: passedType },
+              },
+              {
+                range: {
+                  mag: {
+                    gte: passedMag,
+                  },
+                },
+              },
+              {
+                match: { place: passedLocation },
+              },
+ 
+              {
+                range: {
+                  '@timestamp': {
+                    gte: `now-${passedDateRange}d/d`,
+                    lt: 'now/d',
+                  },
+                },
+              },
+            ],
+          },
+        },
+      },
+    });//search query ends here
+    
+    res.json(body.hits.hits);
+  }
+  sendESRequest();
+});
+
+const PORT = process.env.PORT || 3006;
+
+app.listen(PORT, () => console.group(`Server started on ${PORT}`));
